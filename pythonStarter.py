f156d685b8c2fd33ff72d96fe641882642cdd92d
Joseph Peters <petersjl@rose-hulman.edu>
2022-08-20 13:25:50 -0400

    switched to tabs instead of spaces

--- pythonStarter.py
+++ pythonStarter.py
@@ -2,16 +2,16 @@ from pathlib import Path
 from time import perf_counter as timer
 
 def parseInput():
-    path = Path(__file__).parent / "../input.txt"
-    input = path.open().read()
-    return input
+	path = Path(__file__).parent / "../input.txt"
+	input = path.open().read()
+	return input
 
 def main():
-    input = parseInput()
-    return
+	input = parseInput()
+	return
 
 if __name__ == '__main__':
-    start = timer()
-    main()
-    end = timer()
-    print(f'Ran in {end - start} seconds')
+	start = timer()
+	main()
+	end = timer()
+	print(f'Ran in {end - start} seconds')
