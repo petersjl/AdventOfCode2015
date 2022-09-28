04277e6e838b3df59eaa51b73cda7c0a3dcac32d
Joseph Peters <petersjl@rose-hulman.edu>
2022-09-28 15:22:36 -0400

    Day 16 Part 2

---
+++ 16/Python/Part2.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+import re
+from time import perf_counter as timer
+
+def parseInput():
+	path = Path(__file__).parent / "../input.txt"
+	input = list(map(lambda s: re.split(": |, | ", s.strip()), path.open().read().split('\n')))
+	aunts = {}
+	for x in input:
+		aunts[x[1]] = {}
+		facts = x[2:]
+		i = 0
+		while i < len(facts):
+			aunts[x[1]][facts[i]] = int(facts[i + 1])
+			i += 2
+	path = Path(__file__).parent / "../ticket.txt"
+	input = list(map(lambda s: re.split(": |, | ", s.strip()), path.open().read().split('\n')))
+	knowns = {}
+	for x in input:
+		knowns[x[0]] = int(x[1])
+	return (aunts, knowns)
+
+def main():
+	(aunts, knowns) = parseInput()
+	aunt = findAunt(aunts, knowns)
+	if aunt == -1: print('Could not find aunt')
+	else: print(f'Aunt Sue {aunt} gave the gift')
+
+	return
+
+def findAunt(aunts:dict[str,dict[str,int]], knowns:dict[str,int]) -> int:
+	for aunt in aunts.keys():
+		a = aunts[aunt]
+		if checkAunt(a, knowns): return int(aunt)
+	return -1
+
+def checkAunt(aunt:dict[str,int], knowns:dict[str,int]) -> bool:
+	for k in aunt.keys():
+		match k:
+			case "cats" | "trees":
+				if aunt[k] <= knowns[k]: return False
+			case "pomeranians" | "goldfish":
+				if aunt[k] >= knowns[k]: return False
+			case _ :
+				if aunt[k] != knowns[k]: return False
+	return True
+		
+
+if __name__ == '__main__':
+	start = timer()
+	main()
+	end = timer()
+	print(f'Ran in {end - start} seconds')
